a63b8e8b8dc14b7ce9f5bb53fc392d2e5116c2ed
rayark1 <max980203@snu.ac.kr>
2024-02-15 23:16:33 +0900

    feat: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/waruru/areyouhere/AreYouHereApplication.java
+++ src/main/java/com/waruru/areyouhere/AreYouHereApplication.java
@@ -2,7 +2,9 @@ package com.waruru.areyouhere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AreYouHereApplication {
 
