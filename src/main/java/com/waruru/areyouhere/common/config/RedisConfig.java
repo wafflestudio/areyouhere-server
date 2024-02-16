72b87a32e8391e785f9d0210cddfbacf98303b98
SHEOMM <esh0821@snu.ac.kr>
2024-02-17 02:09:09 +0900

    feat: Redis 설정 및 AuthCode Repository 구현

--- src/main/java/com/waruru/areyouhere/common/config/RedisConfig.java
+++ src/main/java/com/waruru/areyouhere/common/config/RedisConfig.java
@@ -8,9 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableCaching
 @Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
---
+++ src/main/java/com/waruru/areyouhere/session/service/AuthCodeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.waruru.areyouhere.session.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCodeServiceImpl implements AuthCodeService{
+
+}
