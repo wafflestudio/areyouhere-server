16bee4cfb7346b18df32d9e91180859d6e64c274
rayark1 <max980203@snu.ac.kr>
2024-02-16 21:10:02 +0900

    feat: Random을 이용한 알파벳-숫자 랜덤 생성기 구현

---
+++ src/main/java/com/waruru/areyouhere/common/utils/AlphanumericIdGenerator.java
@@ -0,0 +1,18 @@
+package com.waruru.areyouhere.common.utils;
+
+import java.util.Random;
+
+public class AlphanumericIdGenerator implements RandomIdentifierGenerator {
+    private static final String ALPHANUMERIC_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private final Random random = new Random();
+
+    @Override
+    public String generateRandomIdentifier(int length) {
+        StringBuilder builder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(ALPHANUMERIC_CHARACTERS.length());
+            builder.append(ALPHANUMERIC_CHARACTERS.charAt(index));
+        }
+        return builder.toString();
+    }
+}
