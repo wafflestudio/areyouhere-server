686c91f68c4c87520e23538f8316cc2c5bb00777
rayark1 <max980203@snu.ac.kr>
2024-02-17 05:48:59 +0900

    fix: build test 제거

--- src/test/java/com/waruru/areyouhere/AreYouHereApplicationTests.java
+++ src/test/java/com/waruru/areyouhere/AreYouHereApplicationTests.java
@@ -5,9 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AreYouHereApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
