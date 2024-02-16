eeb74bda48e55c43d37b96da6a369a0e1744d9a0
SHEOMM <esh0821@snu.ac.kr>
2024-02-17 02:10:19 +0900

    feat: Course DashBoard Controller 구현

---
+++ src/main/java/com/waruru/areyouhere/session/controller/DashBoardController.java
@@ -0,0 +1,39 @@
+package com.waruru.areyouhere.session.controller;
+
+
+import com.waruru.areyouhere.common.annotation.LoginRequired;
+import com.waruru.areyouhere.session.dto.CurrentSessionResponseDto;
+import com.waruru.areyouhere.session.service.SessionService;
+import com.waruru.areyouhere.session.service.dto.CurrentSessionDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(DashBoardController.DASHBOARD)
+public class DashBoardController {
+
+    public static final String DASHBOARD = "/api/dashboard";
+
+    private final SessionService sessionService;
+
+    @LoginRequired
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CurrentSessionResponseDto> getCurrentSessionInfo(@PathVariable Long courseId) {
+        CurrentSessionDto currentSessionInfo = sessionService.getCurrentSessionInfo(courseId);
+        CurrentSessionResponseDto currentSessionResponseDto = CurrentSessionResponseDto.builder()
+                .authCode(currentSessionInfo.getAuthCode())
+                .sessionName(currentSessionInfo.getSessionName())
+                .sessionTime(currentSessionInfo.getSessionTime())
+                .build();
+        return ResponseEntity.ok(currentSessionResponseDto);
+    }
+
+
+
+}
