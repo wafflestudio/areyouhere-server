41a75b23fa8991fb7a4807cb656db9fe0d6d0bf0
SHEOMM <esh0821@snu.ac.kr>
2024-02-16 14:57:19 +0900

    feat: Redis 환경 설정

---
+++ src/main/java/com/waruru/areyouhere/common/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.waruru.areyouhere.common.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private String port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(Integer.parseInt(port));
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+
+}
