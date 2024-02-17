f94a3d1ca9dc893973e38d2960ab52564722e2d9
HEESEUNG <max980203@snu.ac.kr>
2024-02-18 02:18:15 +0900

    feat/add-websecurity-config: 빈형식으로 websecurity-config 추가 (#23)

--- src/main/java/com/waruru/areyouhere/common/config/WebSecurityConfig.java
+++ src/main/java/com/waruru/areyouhere/common/config/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.waruru.areyouhere.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -13,5 +17,18 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 기능 비활성화
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/**").permitAll() // '/api/**' 경로에 대한 요청은 인증을 요구하지 않음
+                        .anyRequest().authenticated()) // 그 외의 모든 요청은 인증 필요
+                .formLogin(form -> form.disable()) // 폼 로그인 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable()); // HTTP 기본 인증 비활성화
+
+        return http.build();
     }
 }
