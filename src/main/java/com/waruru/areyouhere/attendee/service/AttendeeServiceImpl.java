398700719ca433604b72de2ae5b60b5bcbc9571d
SHEOMM <esh0821@snu.ac.kr>
2024-02-17 20:20:13 +0900

    feat: 수업 참가자 상세 페이지 service, repository 구현

--- src/main/java/com/waruru/areyouhere/attendee/service/AttendeeServiceImpl.java
+++ src/main/java/com/waruru/areyouhere/attendee/service/AttendeeServiceImpl.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Service;
 import com.waruru.areyouhere.attendance.domain.repository.AttendanceRepository;
 import com.waruru.areyouhere.attendee.domain.entity.Attendee;
 import com.waruru.areyouhere.attendee.domain.repository.AttendeeRepository;
+import com.waruru.areyouhere.attendee.domain.repository.dto.ClassAttendeeInfo;
 import com.waruru.areyouhere.attendee.domain.repository.dto.SessionAttendeeInfo;
+import com.waruru.areyouhere.attendee.service.dto.ClassAttendees;
 import com.waruru.areyouhere.attendee.service.dto.SessionAttendees;
 import java.util.Collections;
 import java.util.List;
@@ -40,17 +42,18 @@ public class AttendeeServiceImpl implements AttendeeService{
     }
 
 
-    // 그냥 service에서 throw로 exception 던지고 no content 204 반환하는게 좋지 않으련지?
-    public List<SessionAttendees> getSessionAttendeesIfExistsOrEmpty(Long sessionId){
-        List<SessionAttendeeInfo> sessionAttendees = attendeeRepository.findSessionAttendees(sessionId);
-        return sessionAttendees == null || sessionAttendees.isEmpty() ?
-                Collections.emptyList()
-                : sessionAttendees.stream().map(sessionAttendee -> SessionAttendees.builder()
-                        .attendeeName(sessionAttendee.getAttendeeName())
-                        .attendanceStatus(sessionAttendee.getAttendanceStatus())
-                        .attendanceTime(sessionAttendee.getAttendanceTime())
-                        .build()).toList();
-    }
+    public List<ClassAttendees> getClassAttendeesIfExistsOrEmpty(Long courseId){
+        List<ClassAttendeeInfo> classAttendancesInfos = attendeeRepository.getClassAttendancesInfo(courseId);
 
+        return classAttendancesInfos == null || classAttendancesInfos.isEmpty() ?
+                Collections.emptyList()
+                : classAttendancesInfos.stream().map( classAttendancesInfo -> ClassAttendees.builder()
+                        .name(classAttendancesInfo.getName())
+                        .attendance(classAttendancesInfo.getAttendance())
+                        .absenece(classAttendancesInfo.getAbsence())
+                        .build()
+                ).toList();
+    }
+     
 
 }
