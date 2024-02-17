f3e1db01b55c5481980c24d1df02c9354b5bd14a
HEESEUNG <max980203@snu.ac.kr>
2024-02-18 03:22:32 +0900

    Feat/api-endpoint-setup (#26)

--- src/main/java/com/waruru/areyouhere/course/controller/CourseController.java
+++ src/main/java/com/waruru/areyouhere/course/controller/CourseController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/courses")
 @RequiredArgsConstructor
 public class CourseController {
 
