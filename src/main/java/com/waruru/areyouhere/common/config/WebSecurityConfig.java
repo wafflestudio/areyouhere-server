8a46a7a208bde5848e31eafd1e55a5ddfa06d78c
rayark1 <max980203@snu.ac.kr>
2024-02-18 04:39:38 +0900

    feat: WebSecurityConfig

--- src/main/java/com/waruru/areyouhere/common/config/WebSecurityConfig.java
+++ src/main/java/com/waruru/areyouhere/common/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,12 +23,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 보호 기능 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 기능 비활성화
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/**").permitAll() // '/api/**' 경로에 대한 요청은 인증을 요구하지 않음
-                        .anyRequest().authenticated()) // 그 외의 모든 요청은 인증 필요
-                .formLogin(form -> form.disable()) // 폼 로그인 비활성화
-                .httpBasic(httpBasic -> httpBasic.disable()); // HTTP 기본 인증 비활성화
+                        .requestMatchers("/**").permitAll())
+                .formLogin(AbstractHttpConfigurer::disable); // 폼 로그인 비활성화
 
         return http.build();
     }
