38c78aa9319d0bfa87ff9cde2bacc6bc7b142c29
SHEOM <esh0821@snu.ac.kr>
2024-06-29 00:33:04 +0900

    Fix/Error-Log 서버 오류만 전송 (#59)

--- src/main/java/com/waruru/areyouhere/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/waruru/areyouhere/common/error/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import net.gpedro.integrations.slack.SlackApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,12 +17,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-  private final SlackApi slackApi;
-
-  public GlobalExceptionHandler(@Value("${spring.slack.webhook}") String webhook) {
-    this.slackApi = new SlackApi(webhook);
-  }
 
   @SlackNotification
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
---
+++ src/main/java/com/waruru/areyouhere/common/utils/Ordered.java
@@ -0,0 +1,7 @@
+package com.waruru.areyouhere.common.utils;
+
+public interface Ordered extends org.springframework.core.Ordered {
+    int SECOND_VALUE = 2;
+
+    int THIRD_VALUE = 3;
+}
