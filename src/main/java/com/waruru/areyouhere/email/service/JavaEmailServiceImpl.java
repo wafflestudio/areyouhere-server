819377c52704938adb60e76e1f1f674f7d82f6fd
SHEOMM <esh0821@snu.ac.kr>
2024-08-08 16:57:46 +0900

    feat: 이메일 인증 서비스 OCI 방식 시도

--- src/main/java/com/waruru/areyouhere/email/service/JavaEmailServiceImpl.java
+++ src/main/java/com/waruru/areyouhere/email/service/JavaEmailServiceImpl.java
@@ -2,21 +2,31 @@ package com.waruru.areyouhere.email.service;
 
 import com.waruru.areyouhere.email.domain.MessageHolder;
 import com.waruru.areyouhere.email.domain.MessageTemplate;
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@ConfigurationProperties(prefix = "spring.mail")
 @Slf4j
 public class JavaEmailServiceImpl implements EmailService {
-    private final JavaMailSender emailSender;
 
-    @Value("${spring.mail.from}")
     private String from;
+    private String username;
+    private String password;
+    private String host;
+    private int port;
+
 
 
     public void sendVerifyEmail(String to, String title, String verificationLink, MessageTemplate messageTemplate) {
@@ -24,17 +34,55 @@ public class JavaEmailServiceImpl implements EmailService {
         sendSimpleMessage(to, messageHolder.getTitle(), messageHolder.getContents());
     }
 
-    private void sendSimpleMessage(String to, String title, String content) {
+    private void sendSimpleMessage(String to, String title, String content)  {
         if (from == null || from.isEmpty()) {
             log.info("Email text Test: " + content);
             return;
         }
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(title);
-        message.setText(content);
-        emailSender.send(message);
+
+        Session session = getSession();
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            setMessage(to, title, content, msg);
+            doSend(session, msg);
+
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private void doSend(Session session, MimeMessage msg) throws MessagingException {
+        Transport transport = session.getTransport();
+        transport.connect(host, username, password);
+        transport.sendMessage(msg, msg.getAllRecipients());
+        transport.close();
+    }
+
+    private void setMessage(String to, String title, String content, MimeMessage msg)
+            throws MessagingException, UnsupportedEncodingException {
+        msg.setFrom(new InternetAddress(from, from));
+        InternetAddress internetAddress = new InternetAddress(to);
+        internetAddress.validate();
+        msg.setRecipient(RecipientType.TO, internetAddress);
+        msg.setSubject(title);
+        msg.setContent(content, "text/html");
+        msg.setHeader("X-SES-CONFIGURATION-SET", "ConfigSet");
+    }
+
+    private Session getSession(){
+        Properties emailProps = System.getProperties();
+        emailProps.put("mail.transport.protocol", "smtp");
+        emailProps.put("mail.smtp.port", port);
+        emailProps.put("mail.smtp.starttls.required", "true");
+        emailProps.put("mail.smtp.auth.login.disable", "true");
+        emailProps.put("mail.smtp.starttls.enable", "true");
+        emailProps.put("mail.smtp.auth", "true");
+
+        return Session.getDefaultInstance(emailProps);
+
     }
 
 }
