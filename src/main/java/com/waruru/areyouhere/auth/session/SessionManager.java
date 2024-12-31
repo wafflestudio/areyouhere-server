65e9a27a8cd8b4961f368e353b052b178f1e13f6
SHEOMM <esh0821@snu.ac.kr>
2024-12-31 21:03:00 +0900

    fix: circular dependency 해결

--- src/main/java/com/waruru/areyouhere/auth/session/SessionManager.java
+++ src/main/java/com/waruru/areyouhere/auth/session/SessionManager.java
@@ -18,7 +18,7 @@ public class SessionManager {
 
     private final HttpSession httpSession;
 
-    private static final String LOG_ID = "logId";
+    public static final String LOG_ID = "logId";
 
     public void createSession(Long managerId){
         LoginUser loginUser = new LoginUser(managerId);
--- src/main/java/com/waruru/areyouhere/manager/service/OAuth2Service.java
+++ src/main/java/com/waruru/areyouhere/manager/service/OAuth2Service.java
@@ -1,27 +1,30 @@
 package com.waruru.areyouhere.manager.service;
 
-import com.waruru.areyouhere.auth.session.SessionManager;
+import com.waruru.areyouhere.auth.entity.LoginUser;
 import com.waruru.areyouhere.common.utils.random.RandomIdentifierGenerator;
+import com.waruru.areyouhere.manager.domain.entity.Manager;
 import com.waruru.areyouhere.manager.domain.repository.ManagerRepository;
-import com.waruru.areyouhere.manager.domain.repository.VerifyCodeRepository;
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import static com.waruru.areyouhere.auth.session.SessionManager.LOG_ID;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class OAuth2Service extends DefaultOAuth2UserService {
     private final ManagerRepository managerRepository;
-    private final ManagerService managerService;
-    private final SessionManager sessionManager;
-    private final VerifyCodeRepository verifyCodeRepository;
+    private final HttpSession httpSession;
     private final RandomIdentifierGenerator randomIdentifierGenerator;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
@@ -32,12 +35,20 @@ public class OAuth2Service extends DefaultOAuth2UserService {
         managerRepository.findManagerByEmail(email)
                 .ifPresentOrElse(
                         manager -> {
-                            sessionManager.createSession(manager.getId());
+                            LoginUser loginUser = new LoginUser(manager.getId());
+                            httpSession.setAttribute(LOG_ID, loginUser);
                         },
                         () -> {
                             String password = randomIdentifierGenerator.generateRandomIdentifier(10);
-                            verifyCodeRepository.saveAndGetCode(email);
-                            managerService.signUp(email, password, name);
+                            Manager manager = managerRepository.save(
+                                    Manager.builder()
+                                            .email(email)
+                                            .name(name)
+                                            .password(passwordEncoder.encode(password))
+                                            .build()
+                            );
+                            LoginUser loginUser = new LoginUser(manager.getId());
+                            httpSession.setAttribute(LOG_ID, loginUser);
                         }
                 );
 
